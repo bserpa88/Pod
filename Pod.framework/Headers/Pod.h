5f00a8aeae9bb45af8af281b27dc9ba5063f0fd4
Bryan Serpa <bryan.serpa@honeywell.com>
2021-06-22 21:28:13 -0700

    first commit adding framework

---
+++ Pod.framework/Headers/Pod.h
@@ -0,0 +1,19 @@
+//
+//  Pod.h
+//  Pod
+//
+//  Created by H363660 on 6/22/21.
+//  Copyright © 2021 H363660. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pod.
+FOUNDATION_EXPORT double PodVersionNumber;
+
+//! Project version string for Pod.
+FOUNDATION_EXPORT const unsigned char PodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pod/PublicHeader.h>
+
+
